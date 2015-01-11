401cfd7aaa7c8e075500e5cdfd333e3e79d42fad
Raymond <raymondctc@me.com>
2015-01-11 14:10:49 +0800

    path for submodules update

--- app/src/main/java/dnomyar/combo/huds/ProgressBar.java
+++ app/src/main/java/dnomyar/combo/huds/ProgressBar.java
@@ -3,6 +3,8 @@ package dnomyar.combo.huds;
 import org.andengine.entity.primitive.Rectangle;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
+import dnomyar.combo.utils.ColorUtils;
+
 /**
  * Created by Raymond on 2015-01-10.
  */
@@ -17,5 +19,10 @@ public class ProgressBar extends Rectangle {
 
     public void setProgress(float progress) {
         this.setWidth(this.progressWidth * progress);
+        if (progress <= 0.50 && progress > 0.25) {
+            this.setColor(ColorUtils.getDefaultYellow());
+        } else if (progress <= 0.25) {
+            this.setColor(ColorUtils.getDefaultRed());
+        }
     }
 }
